e57a422f84bac9911db6af497f59de821407c503
Pedro <pedrosss7770@gmail.com>
2026-05-11 19:33:50 +0200

    feat: add user registration foundation

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { env } from './env/index.js';
+import { app } from './app.js';
+
+app.listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+}).then(() => {
+    console.log('Server is running on http://localhost:' + env.PORT);
+})
