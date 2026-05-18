16ea69ff5ac7cf2c25db9e0f6017493502bdd8a3
Pedro <pedrosss7770@gmail.com>
2026-05-18 18:36:58 +0200

    feat: add unit tests for register use case with in-memory repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,12 +1,17 @@
+import type { UsersRepository } from "@/repositories/users-repository.js";
+import type { User } from "@prisma/client";
 import { hash } from "bcryptjs";
 import { UserAlreadyExistsError } from "./errors/user-already-exists-error.js";
-import type { UsersRepository } from "@/repositories/users-repository.js";
 
 
 interface RegisterUseCaseRequest {
     name: string;
     email: string;
     password: string;
+}
+
+interface RegisterUseCaseResponse {
+    user: User;
 }
 
 export class RegisterUseCase {
@@ -26,10 +31,11 @@ export class RegisterUseCase {
 
         const password_hash = await hash(password, 6)
 
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash,
         })
+        return user
     }
 }
